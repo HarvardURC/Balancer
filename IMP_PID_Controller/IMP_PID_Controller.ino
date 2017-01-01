00e49a5ab289d50e80b2b818c8b8fd4f38a9717a
ameguid123 <adham_meguid@college.harvard.edu>
2017-01-01 18:06:32 -0500

    Graphing Functionality (visualization CREDIT: farrellf) Added functionality for convenient data visualization. Code changes include new print statements in comma separated format. Added a new file, TelemetryViewer, created by (CREDIT) farrellf (https://github.com/farrellf/Telemetry-Viewer), which does an excellent job of providing flexible graphing of data from Arduino.

--- IMP_PID_Controller/IMP_PID_Controller.ino
+++ IMP_PID_Controller/IMP_PID_Controller.ino
@@ -1,5 +1,6 @@
 // note: to use serial in to change pid constants mid-test,
-	// format to enter the values in: (kP,kI,kD)
+	// format to enter the values in:
+	//kP,kI,kD
 
 // TODO: replace DUALVNH5019 lib
 // TODO: seperate constants for each motor
@@ -15,7 +16,6 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 #include <PID_v1.h>
-
 
 /*
   CREDIT: ARDUINO BNO055 EXAMPLE CODE
@@ -38,12 +38,16 @@ DualVNH5019MotorShield md;
 
 /* PID object and required globals */
 
-double output, input, setPoint;
+double output, pitch, setPoint;
 
-PID pid(&input, &output, &setPoint, 5.3, 0, 0.18, AUTOMATIC);
+// pid consts used with serial input to modify constants
+float kP = 5.3;
+float kI = 0;
+float kD = 0.18;
 
-// string versions of pid consts used with serial input to modify constants
-String kP, kI, kD;
+PID pid(&pitch, &output, &setPoint, kP, kI, kD, AUTOMATIC);
+
+
 
 // was 65/60
 float MOTORSLACK_1 = 65;	// Compensate for motor slack range (low PWM values which result in no motor engagement)
@@ -103,10 +107,10 @@ void loop()
 		bno.getEvent(&event);
 
 		// get the z orientation
-		input = event.orientation.z;
+		pitch = event.orientation.z;
 
 		// if we're out of control stop motors.
-		if (abs(input) > 45)
+		if (abs(pitch) > 45)
 		{
 			md.setM1Speed(0);
 			md.setM2Speed(0);
@@ -115,7 +119,7 @@ void loop()
 		{
 			// commented out option to create a deadzone when robot within 1 deg of ctr
 			/*
-			if (input < 1)
+			if (pitch < 1)
 			{
 				md.setM1Speed(0);
 				md.setM2Speed(0);
@@ -152,28 +156,52 @@ void loop()
 		int delimIndx2 = inSerialData.indexOf(",", delimIndx+1);
 
 		// use substrings to get our constants
-		kP = inSerialData.substring(0, delimIndx);
-		kI = inSerialData.substring(delimIndx+1, delimIndx2);
-		kD = inSerialData.substring(delimIndx2);
+		kP = inSerialData.substring(0, delimIndx).toFloat();
+		kI = inSerialData.substring(delimIndx+1, delimIndx2).toFloat();
+		kD = inSerialData.substring(delimIndx2).toFloat();
 		// set our new PID constantss
-		pid.SetTunings(kP.toFloat(), kI.toFloat(), kD.toFloat());
+		pid.SetTunings(kP, kI, kD);
 	}
 
-	// testing + debugging data
-	/*Serial.print(" Z: ");
-	  Serial.print(input);
-	  Serial.print(" output: ");
-	  Serial.print(output);
-	  Serial.print(" setPoint: ");
+	// graphing data
+	// CREDIT: farrellf, https://github.com/farrellf/Telemetry-Viewer
 
-	  Serial.print(setPoint);
-	  Serial.print(" end dir: ");
-	  Serial.println(enc_direction);*/
+	// for basic graphing of pitch vs setpoint and tuning
+	// Serial.print(pitch); Serial.print(",");
+	// Serial.println(setPoint);
 
-	//  Serial.print(millis()); // print the time in milliseconds since the program started
-	//  Serial.print(',');
-	//  Serial.println(euler.z()); // print to serial
+	// for more detailed debug
+	imu::Vector<3> accel = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
+	imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
+	imu::Vector<3> mag = bno.getVector(Adafruit_BNO055::VECTOR_MAGNETOMETER);
 
+	/* Explaining the below
+	   Arduino does not support sprintf with floats
+	   (which would have been a much better alternative to the below)
+	   dtostrf is nice, but we'd have to do that for every one of the
+	   variables, making lots of temporary buffers and it would have taken
+	   a lot of space.
+	   A user created function, as at:
+	   https://gist.github.com/asheeshr/9004783
+	   could work well, but I can't test until back on campus, so for now
+	   the messy code below until we get the graphing visualization to work.
+	*/
+	Serial.print(accel.x()); Serial.print(",");
+	Serial.print(accel.y()); Serial.print(",");
+	Serial.print(accel.z()); Serial.print(",");
+	Serial.print(gyro.x());  Serial.print(",");
+	Serial.print(gyro.y());  Serial.print(",");
+	Serial.print(gyro.z());  Serial.print(",");
+	Serial.print(mag.x());   Serial.print(",");
+	Serial.print(mag.y());	 Serial.print(",");
+	Serial.print(mag.z());	 Serial.print(",");
+	Serial.print(pitch); 	 Serial.print(",");
+	Serial.print(setPoint);  Serial.print(",");
+	Serial.print(output);    Serial.print(",");
+	Serial.print(kP);        Serial.print(",");
+	Serial.print(kI);		 Serial.print(",");
+	Serial.print(kI);		 Serial.print(",");
+	Serial.println(kD);
 }
 
 /*CREDIT ManpreetSingh80 on Github
