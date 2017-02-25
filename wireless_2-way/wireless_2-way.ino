cd41da65289564c972f0cf71cbfdb9f266aa4a8f
ameguid123 <adham_meguid@college.harvard.edu>
2017-02-25 17:54:36 -0500

    2-Way Wireless Communication Functional Currently uses joystick's button sensor. Attach the button sensor to one of the two Arduinos. When the button is clicked, the arduino attached with that button will stop listening for communication, enter write mode, and trasmit "Hi balancer" over to the other Arduino. Once the message has finished, the joystick Arduino will exit write mode, and re-commence listening for messages. Both Arduinos use the same pipe to both send and receive.

--- wireless_2-way/wireless_2-way.ino
+++ wireless_2-way/wireless_2-way.ino
@@ -4,53 +4,88 @@
 
 // http://maniacbug.github.io/RF24/classRF24.html#a391eb0016877ec7486936795aed3b5ee
 // radio variables
-RF24 radio(7, 8);
-const uint64_t rxAddr = 0xF0F0F0F0AA;
+RF24 radio(9, 10);
+const uint64_t pipe = 0xF0F0F0F0AA;
 
 // joystick variables
-
+int VRx = A0;
+int VRy = A1;
+int x_pos, y_pos, past_x_pos, past_y_pos, button_state, past_button;
+int button_pin = 7;
 
 void setup()
 {
+	button_state = 1;
+	past_button = -1;
 	Serial.begin(115200);
+	// radio setup
  	radio.begin();
   	radio.setRetries(15, 10); // delay of 15ms, 10 retries
-  	radio.openReadingPipe(1, rxAddr);
+  	radio.openReadingPipe(1, pipe);
   	radio.startListening(); 
-  	radio.printDetails();       
+  	radio.printDetails();  
+
+  	// joystick setup
+	pinMode(VRx, INPUT);
+    pinMode(VRy, INPUT);
+    
+  	// pull-up resistor joystick button
+  	pinMode(button_pin, INPUT_PULLUP); 
  }
+
 
 void loop()
 {
-	listen();
+	listen_();
 	delay(10);
-	broadcast();
+	transmit();
 }
 
 // listen for incoming data
-void listen() 
+void listen_() 
 {
 	if (radio.available() )
-	{
-		// payload status
-		bool payload_receive = false;
-		
+	{	
 		// buffer to store payload
-		char text[32] = {0};
+		char command[32] = {0};
 
-		// wait until successful payload delivery
-		while (!end_receive)
-		{
-			// reading payload
-			payload_receive = radio.read(&command, sizeof(command) );
-		}
-		Serial.print(command);
+		// reading payload
+		radio.read(&command, sizeof(command) );
+		Serial.println(command);
+	}
 }
 
-
-void broadcast()
+void transmit()
 {
+  //x_pos = analogRead(VRx);
+  //Serial.println(x_pos);
+  button_state = digitalRead(button_pin);
+  
+  // y_pos = analogRead(VRy);
+  // something new to write
+  //if (x_pos != past_x_pos || y_pos != past_y_pos)
+ // {
+ 
+  //	past_x_pos = x_pos;
+ // 	past_y_pos = y_pos;
+
+	if (button_state != past_button)
+	{
+		past_button = button_state;
+  		const char text[] = "Hi balancer";
 	
+	  	// can't listen while writing 
+	  	radio.stopListening(); 
+	  	radio.openWritingPipe(pipe);
+	  	radio.write(&text, sizeof(text) );
+	  	// done writing, back to reading pipe
+	    radio.openReadingPipe(1,pipe); 
+	    // begin listening again
+	    radio.startListening();
+	    // not spamming comms
+	    delay(20);
+	}
+
 }
-}
+
 
