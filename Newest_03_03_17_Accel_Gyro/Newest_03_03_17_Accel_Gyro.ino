1b4dbadf6d3a4303a6f8df6613ce6b3efc79be25
ameguid123 <adham_meguid@college.harvard.edu>
2017-03-03 20:38:33 -0500

    Complementary Filter Running. Currently, robot response time seems slow. Will investigate the author of the used complementary filter's choice of time units, which seems strange (calling Arduino millis() and storing in integer)

--- Newest_03_03_17_Accel_Gyro/Newest_03_03_17_Accel_Gyro.ino
+++ Newest_03_03_17_Accel_Gyro/Newest_03_03_17_Accel_Gyro.ino
@@ -1,5 +1,4 @@
 
-// NEW CREDIT: http://robottini.altervista.org/kalman-filter-vs-complementary-filter
 // http://forum.arduino.cc/index.php?topic=58048.0
 #include "DualVNH5019MotorShield.h"
 #include <Wire.h>
@@ -20,46 +19,29 @@ Adafruit_BNO055 bno = Adafruit_BNO055(55, 0x29); // Need to call constructor dif
 DualVNH5019MotorShield md;
 
 /* PID constants and required globals */
-double output, pitch, setPoint, send_pitch;
+double output, setPoint, send_pitch;
 
 //
-int gyro_angle;
-int acc_angle;
-double gyro_rate;
-/*
-int val_sensor;
-float accZ = 0;
-float accY = 0;
-float x1=0;
-float y1=0;
-float x2=0;
-float y2=0;
-float actAngle=0;  
-float actAngleC=0;  
-float actAngleRG=0;
-float actAngleRA=0;
-float actAngleC2=0;
-*/
+float acc_angle;
+float gyro_rate;
+double actAngleC = 0;  
+
 unsigned long current_time = 0;               //timer
 unsigned long  delta_t = 0;            //delta time or how long it takes to execute data acquisition 
 int lastLoopTime=5;
-float x_angle=0;
 float x_angleC=0;
-float x_angleRG=0;
-float  x_angleRA=0;
-float x_angle2C=0;
 
 float MOTORSLACK_1 = 30;	// Compensate for motor slack range (low PWM values which result in no motor engagement)
 float MOTORSLACK_2 = 30;	// Compensate for motor slack range (low PWM values which result in no motor engagement)
 
-float Kp = 0;   // crrent best @ setpoint 0.35
+float Kp = 30;   // crrent best @ setpoint 0.35
 float Ki = 0;
 float Kd = 0;
 
 float tau = 0.075;
 float a = 0.0;
 
-PID pid(&pitch, &output, &setPoint, Kp, Ki, Kd, AUTOMATIC);
+PID pid(&actAngleC, &output, &setPoint, Kp, Ki, Kd, AUTOMATIC);
 
 
 
@@ -107,19 +89,13 @@ void loop()
 	acc_angle = getAccAngle() * 180/pi;
 	imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
 
-	gyro_rate = gyro.x();
+	gyro_rate = (gyro.x() * 4068) / 71; // convert gyro in radians to degrees
+	actAngleC = complementary(acc_angle, gyro_rate, delta_t);    // calculate Absolute Angle with complementary filter
 	
-	
-	
-
-		/*
-		// get new gyro data
-		Filter();
-
 		// if z has changed update pos. and find which direction we rotated
 		
 		// if we're out of control stop motors.
-		if (abs(pitch) > 45)
+		if (abs(actAngleC) > 45)
 		{
 			md.setM1Speed(0);
 			md.setM2Speed(0);
@@ -127,42 +103,18 @@ void loop()
 		else
 		{
 			pid.Compute();
-			double output1 = compensate_slack(output, 1); //M1
-			double output2 = compensate_slack(output, 0); //M2
+			//double output1 = compensate_slack(output, 1); //M1
+			//double output2 = compensate_slack(output, 0); //M2
 
-			md.setM1Speed(-output1); //26.5
-			md.setM2Speed(output2); // 25
+			md.setM1Speed(-output); //26.5
+			md.setM2Speed(output); // 25
 		}
-		*/
 	
 }
 
+// CREDIT: http://robottini.altervista.org/kalman-filter-vs-complementary-filter
 
-//credit: https://codebender.cc/user/bruno.huang
-/*void Filter()
-{
-	imu::Vector<3> accel = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
-	imu::Vector<3> gyro = bno.getVector(Adafruit_BNO055::VECTOR_GYROSCOPE);
-
-	//credit: https://codebender.cc/user/bruno.huang
-	Angle_Raw = (atan2(accel.y(), accel.z()) * 180 / pi);
-	omega = gyro.x(); // + Gyr_Gain + Gyr_offset
-	unsigned long now = micros();
-	time_change = now - pre_time;
-	pre_time = now;
-	dt = time_change * 0.00001;
-	Angle_Delta = (Angle_Raw - Angle_Filtered) * 0.64;
-  	Angle_Recursive = Angle_Delta * dt + Angle_Recursive;
-  	Angle_Confidence = Angle_Recursive + (Angle_Raw - Angle_Filtered) * 1.6 + omega; //error bar
-  	Angle_Filtered = Angle_Confidence * dt + Angle_Filtered;
-  	pitch = Angle_Filtered;
-	Serial.println(Angle_Filtered); 	
-}
-*/
-/*CREDIT ManpreetSingh80 on Github
-   https://github.com/ManpreetSingh80/CHAPPIE/blob/master/SelfBalance_robot0_66_withoutEEPROM/SelfBalance_robot0_66_withoutEEPROM.ino
-*/
-double getAccAngle()
+float getAccAngle()
 {
 	imu::Vector<3> accel = bno.getVector(Adafruit_BNO055::VECTOR_ACCELEROMETER);
 	return (atan2(accel.y(), accel.z()));
@@ -177,6 +129,9 @@ double complementary(float new_angle, float new_rate, int looptime)
 	return x_angleC;
 }
 
+/*CREDIT ManpreetSingh80 on Github
+   https://github.com/ManpreetSingh80/CHAPPIE/blob/master/SelfBalance_robot0_66_withoutEEPROM/SelfBalance_robot0_66_withoutEEPROM.ino
+*/
 double compensate_slack(double Output, bool A)
 {
 	// Compensate for DC motor non-linear "dead" zone around 0 where small values don't result in movement
